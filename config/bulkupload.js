fc5bf4d1995e28894d9b57f3917422345702a1a2
patilrohit1964 <patilrohit19649@gmail.com>
2025-04-26 23:33:05 +0530

    done

--- config/bulkupload.js
+++ config/bulkupload.js
@@ -1,4 +1,4 @@
-import { collection, doc } from "firebase/firestore";
+import { collection, doc, setDoc } from "firebase/firestore";
 import { restaurants } from "../store/restaurant";
 import { db } from "./firebaseConfig";
 
@@ -8,6 +8,10 @@ const uploadData = async () => {
     for (let i = 0; i < restaurantData.length; i++) {
       const restaurant = restaurantData[i];
       const docRef = doc(collection(db, "restaurants"), `restaurants_${i + 1}`);
+      await setDoc(docRef, restaurant);
+      console.log("data upload");
     }
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+  }
 };
