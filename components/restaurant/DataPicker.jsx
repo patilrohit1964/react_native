00c84628052844875d0b115dfbe7a30712410189
patilrohit1964 <patilrohit19649@gmail.com>
2025-05-03 22:05:30 +0530

    done

--- components/restaurant/DataPicker.jsx
+++ components/restaurant/DataPicker.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { DateTimePickerAndroid } from '@react-native-community/datetimepicker'
+import React, { useState } from 'react'
+import { Touchable, TouchableOpacity } from 'react-native'
+import { View } from 'react-native-web'
 
 const DataPicker = () => {
-  return (
-    <div>DataPicker</div>
-  )
+    const [show, setShow] = useState(false)
+    const [date, setDate] = useState(new Date())
+    const handlePress = () => {
+        setShow(true)
+    }
+    return (
+        <View>
+            <TouchableOpacity onPress={handlePress}>
+                <DateTimePickerAndroid value={date} mode="date" display="default" minimumDate={new Date()} maximumDate={new Date(new Date().setDate(new Date().getDate()))} />
+            </TouchableOpacity>
+        </View>
+    )
 }
 
-export default DataPicker
+export default DataPicker;
