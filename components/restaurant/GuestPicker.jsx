0bf74793c4cf7c5a4ef1c007713d753ed2d0f452
patilrohit1964 <patilrohit19649@gmail.com>
2025-05-04 14:41:04 +0530

    done

--- components/restaurant/GuestPicker.jsx
+++ components/restaurant/GuestPicker.jsx
@@ -5,18 +5,25 @@ const GuestPicker = ({ selectedNumber, setSelectedNumber }) => {
 
 
     const decrement = () => {
-
+        if (selectedNumber > 1) {
+            setSelectedNumber(setSelectedNumber - 1)
+        }
+    }
+    const increment = () => {
+        if (selectedNumber < 12) {
+            setSelectedNumber(setSelectedNumber + 1)
+        }
     }
     return (
         <View className="flex flex-row items-center rounded-lg text-white text-base">
             <TouchableOpacity onPress={decrement} className="rounded">
-                <Text className="text-white text-lg border border-[#f49b33] rounded">-</Text>
+                <Text className="text-white text-lg border border-[#f49b33] rounded-l-lg px-3">-</Text>
             </TouchableOpacity>
-            <Text className="px-3 text-white bg-[#474747] text-lg">
+            <Text className="px-3 text-white bg-[#474747] border-[#474747] text-lg">
                 {selectedNumber}
             </Text>
             <TouchableOpacity onPress={increment} className="rounded">
-                <Text className="text-white text-lg border border-[#f49b33] rounded">+</Text>
+                <Text className="text-white text-lg border border-[#f49b33] rounded-r-lg px-3">+</Text>
             </TouchableOpacity>
         </View>
     )
