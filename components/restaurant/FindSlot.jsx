f27e2165c59180dceed6b3b38c10c0f2a6372da7
patilrohit1964 <patilrohit19649@gmail.com>
2025-05-08 00:15:10 +0530

    done

--- components/restaurant/FindSlot.jsx
+++ components/restaurant/FindSlot.jsx
@@ -9,15 +9,30 @@ const FindSlot = ({ slots, selectedSlot, setSelectedSlot, selectedNumber, data }
     return (
         <View className="flex-1">
             <View className={`flex ${selectedSlot != null && "flex-row"}`}>
-                <TouchableOpacity onPress={handlePress}>
-                    <Text className="text-lg text-center font-semibold bg-[#f49b33] p-2 my-3 mx-2 rounded-lg">Find Slots</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={handlePress}>
-                    <Text className="text-lg text-center font-semibold bg-[#f49b33] p-2 my-3 mx-2 rounded-lg">Find Slots</Text>
-                </TouchableOpacity>
+                <View className={`${selectedSlot != null && 'flex-1'}`}>
+                    <TouchableOpacity onPress={handlePress}>
+                        <Text className="text-lg text-center font-semibold bg-[#f49b33] p-2 my-3 mx-2 rounded-lg">Find Slots</Text>
+                    </TouchableOpacity>
+                </View>
+                {selectedSlot != null && (
+                    <View className="flex-1">
+                        <TouchableOpacity onPress={handlePress}>
+                            <Text className="text-lg text-center text-white font-semibold bg-[#f49b33] p-2 my-3 mx-2 rounded-lg">Book Slots</Text>
+                        </TouchableOpacity>
+                    </View>
+                )}
+            </View>
+            <View>
+                {slotVisible && (
+<View>
+    {slots.map((el,index)=>(
+        
+    ))}
+</View>
+                )
+                }
             </View>
         </View>
     )
 }
-
 export default FindSlot
