f9c224d412b4f071100dd8edc57adb4d3ab057f0
patilrohit1964 <patilrohit19649@gmail.com>
2025-05-08 00:06:27 +0530

    done

--- components/restaurant/FindSlot.jsx
+++ components/restaurant/FindSlot.jsx
@@ -1,12 +1,21 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
 
 const FindSlot = ({ slots, selectedSlot, setSelectedSlot, selectedNumber, data }) => {
+    const [slotVisible, setSlotVisible] = useState(false)
+    const handlePress = () => {
+        setSlotVisible(!slotVisible)
+    }
     return (
-        <View>
-            <Text>
-
-            </Text>
+        <View className="flex-1">
+            <View className={`flex ${selectedSlot != null && "flex-row"}`}>
+                <TouchableOpacity onPress={handlePress}>
+                    <Text className="text-lg text-center font-semibold bg-[#f49b33] p-2 my-3 mx-2 rounded-lg">Find Slots</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={handlePress}>
+                    <Text className="text-lg text-center font-semibold bg-[#f49b33] p-2 my-3 mx-2 rounded-lg">Find Slots</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
