6bf1f7fa26cd82cceb52d675b28aba69eeb68bca
patilrohit1964 <patilrohit19649@gmail.com>
2025-06-02 23:46:09 +0530

    done

--- app/(auth)/signin.jsx
+++ app/(auth)/signin.jsx
@@ -16,6 +16,10 @@ const Signin = () => {
     })
     const auth = getAuth()
     const db = getFirestore()
+    const handleGuest = async () => {
+        await AsyncStorage.setItem("isGuest", "true")
+        router.push("/home")
+    }
     const handleSubmitBar = async (values, { resetForm }) => {
         try {
             setLoading(true)
@@ -91,7 +95,7 @@ const Signin = () => {
                                 or{" "}
                                 <View className="border-b-2 border-[#f49b33] p-2 mb-3 w-24" />{" "}
                             </Text>
-                            <TouchableOpacity className="flex flex-row justify-center mb-5 p-2 items-center" onPress={() => router.push("/home")}>
+                            <TouchableOpacity className="flex flex-row justify-center mb-5 p-2 items-center" onPress={handleGuest}>
                                 <Text className="text-white font-semibold">Be a</Text>
                                 <Text className="text-base font-semibold underline text-[#f49b33]">{" "} Guest User</Text>
                             </TouchableOpacity>
