3773b1a7128b6dcc3165f8aa7e9d12cec35c2706
patilrohit1964 <patilrohit19649@gmail.com>
2025-06-08 22:16:36 +0530

    done

--- app/(tabs)/history.jsx
+++ app/(tabs)/history.jsx
@@ -1,13 +1,25 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView, FlatList } from 'react-native'
 import React from 'react'
+import { useRouter } from 'expo-router';
+import { getFirestore } from 'firebase/firestore';
 
 const History = () => {
+  const [userEmail, setUserEmail] = useState();
+  const [bookings, setBookings] = useState([]);
+  const [loading, setLoading] = useState([]);
+  const router = useRouter();
+  const db = getFirestore()
+  useEffect(() => {
+    const fetchUserEmail = async () => {
+
+    }
+  }, []);
   return (
-    <View>
-      <Text>
-        
-      </Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-[#2b2b2b]">
+      {
+        userEmail ? (<FlatList></FlatList>) : ""
+      }
+    </SafeAreaView>
   )
 }
 
