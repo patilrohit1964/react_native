d4309eb936860adc750ee615eefaf0ece84b1018
patilrohit1964 <patilrohit19649@gmail.com>
2025-06-08 22:44:44 +0530

    done

--- app/(tabs)/history.jsx
+++ app/(tabs)/history.jsx
@@ -18,46 +18,54 @@ const History = () => {
     }
     fetchUserEmail()
   }, []);
-  useEffect(() => {
-    const fetchBookings = async () => {
-      if (userEmail) {
-        try {
-          const bookingCollection = collection(db, "bookings")
-          const bookingQuery = query(bookingCollection, where("email", "==", userEmail))
-          const bookingSnapShot = await getDocs(bookingQuery)
-          const bookingList = bookingSnapShot.docs.map((doc) => ({
-            id: doc.id,
-            ...doc.data()
-          }))
-          setBookings(bookingList)
-        }
-        catch (error) {
-          console.log(error)
-        }
-        finally {
-          setLoading(false)
-        }
+  const fetchBookings = async () => {
+    if (userEmail) {
+      try {
+        const bookingCollection = collection(db, "bookings")
+        const bookingQuery = query(bookingCollection, where("email", "==", userEmail))
+        const bookingSnapShot = await getDocs(bookingQuery)
+        const bookingList = bookingSnapShot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data()
+        }))
+        setBookings(bookingList)
       }
-      setLoading(false)
+      catch (error) {
+        console.log(error)
+      }
+      finally {
+        setLoading(false)
+      }
     }
+    setLoading(false)
+  }
+  useEffect(() => {
     fetchBookings();
   }, [userEmail]);
   return loading ? "" : (
     <SafeAreaView className="flex-1 bg-[#2b2b2b]">
       {
-        userEmail ? (<FlatList data={bookings} keyExtractor={(item) => item.id} renderItem={({ item }) => (
-          <View className="p-4 border-b border-[#fb9b33]">
-            <Text className="text-white">Date:{item.date}</Text>
-            <Text className="text-white">Slot:{item.slot}</Text>
-            <Text className="text-white">Guest:{item.guests}</Text>
-            <Text className="text-white">Restaurant:{item.restaurant}</Text>
-            <Text className="text-white">Email:{item.email}</Text>
-          </View>
-        )}
+        userEmail ? (<FlatList
+          onRefresh={fetchBookings}
+          refreshing={loading}
+          data={bookings}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <View className="p-4 border-b border-[#fb9b33]">
+              <Text className="text-white">Date:{item.date}</Text>
+              <Text className="text-white">Slot:{item.slot}</Text>
+              <Text className="text-white">Guest:{item.guests}</Text>
+              <Text className="text-white">Restaurant:{item.restaurant}</Text>
+              <Text className="text-white">Email:{item.email}</Text>
+            </View>
+          )}
           contentContainerStyle={{ paddingBottom: 20 }}
         />) : (
-          <View>
-            <Text>Please Sign in to view your booking history</Text>
+          <View className="flex-1 justify-center items-center">
+            <Text className="text-white mb-4">Please Sign in to view your booking history</Text>
+            <TouchableOpacity onPress={() => router.push("/signin")} className="flex flex-row items-center">
+              <Text className="text-base font-semibold underline text-[#f49b33]">Sign up</Text>
+            </TouchableOpacity>
           </View>
         )
       }
