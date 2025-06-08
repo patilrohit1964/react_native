78bedeab775919dd3c478fbc4a0d8a5e84aa205a
patilrohit1964 <patilrohit19649@gmail.com>
2025-06-08 22:34:22 +0530

    done

--- app/(tabs)/history.jsx
+++ app/(tabs)/history.jsx
@@ -1,13 +1,13 @@
 import { View, Text, SafeAreaView, FlatList } from 'react-native'
 import React from 'react'
 import { useRouter } from 'expo-router';
-import { collection, getFirestore, query, where } from 'firebase/firestore';
+import { collection, getDocs, getFirestore, query, where } from 'firebase/firestore';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const History = () => {
   const [userEmail, setUserEmail] = useState();
   const [bookings, setBookings] = useState([]);
-  const [loading, setLoading] = useState([]);
+  const [loading, setLoading] = useState(true);
   const router = useRouter();
   const db = getFirestore()
   useEffect(() => {
@@ -24,17 +24,42 @@ const History = () => {
         try {
           const bookingCollection = collection(db, "bookings")
           const bookingQuery = query(bookingCollection, where("email", "==", userEmail))
+          const bookingSnapShot = await getDocs(bookingQuery)
+          const bookingList = bookingSnapShot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data()
+          }))
+          setBookings(bookingList)
         }
         catch (error) {
           console.log(error)
         }
+        finally {
+          setLoading(false)
+        }
       }
+      setLoading(false)
     }
-  }, []);
-  return (
+    fetchBookings();
+  }, [userEmail]);
+  return loading ? "" : (
     <SafeAreaView className="flex-1 bg-[#2b2b2b]">
       {
-        userEmail ? (<FlatList></FlatList>) : ""
+        userEmail ? (<FlatList data={bookings} keyExtractor={(item) => item.id} renderItem={({ item }) => (
+          <View className="p-4 border-b border-[#fb9b33]">
+            <Text className="text-white">Date:{item.date}</Text>
+            <Text className="text-white">Slot:{item.slot}</Text>
+            <Text className="text-white">Guest:{item.guests}</Text>
+            <Text className="text-white">Restaurant:{item.restaurant}</Text>
+            <Text className="text-white">Email:{item.email}</Text>
+          </View>
+        )}
+          contentContainerStyle={{ paddingBottom: 20 }}
+        />) : (
+          <View>
+            <Text>Please Sign in to view your booking history</Text>
+          </View>
+        )
       }
     </SafeAreaView>
   )
